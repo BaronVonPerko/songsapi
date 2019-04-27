be738a1300a4aa92327fad9bb930da07e23186ae
Chris Perko <chris@chrisperko.net>
2019-04-27 13:00:06 -0400

    Episode 3

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -3,40 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using songsapi.Models;
 
 namespace songsapi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class SongsController : ControllerBase
     {
-        // GET api/values
+        // GET api/songs
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Song>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var song = new Song() {
+                Title = "Supermassive Black Hole",
+                Artist = "Muse",
+                CreatedAt = DateTime.Now,
+                Id = 1
+            };
+
+            return new Song[] { song };
         }
 
-        // GET api/values/5
+        // GET api/songs/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return $"the id is {id}";
         }
 
-        // POST api/values
+        // POST api/songs
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/songs/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/songs/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ Models/Song.cs
@@ -0,0 +1,13 @@
+using System;
+namespace songsapi.Models
+{
+    public class Song
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
